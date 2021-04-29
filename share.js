c7cf267e33a8b19237cbc3e9707deb7475f4a883
EliF <elif.dev.kr@gmail.com>
2021-04-29 10:27:56 +0900

    Update share.js

--- share.js
+++ share.js
@@ -3,17 +3,13 @@ module.exports = function() {
     /* https://github.com/EliF-Lee/shareKakao */
     function t() {
         this.h = {
-            ct: "application/x-www-form-urlencoded",
-            cj: "application/json",
-            ua: "KakaoTalk Share Extension/9.1.7"
-        }, this.pa = null, this.mi = null, this.k = {
-            0: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"],
-            1: [10, 2, 3, -4, 20, 73, 47, -38, 27, -22, 11, -20, -22, 37, 36, 54],
-            2: [67, 109, -115, -110, -23, 119, 33, 86, -99, -28, -102, 109, -73, 13, 43, -96, 109, -76, 91, -83, 73, -14, 107, -88, 6, 11, 74, 109, 84, -68, -80, 15],
-            3: "dkljleskljfeisflssljeif "
-        }, this.p = {
-            pu: "hw.perferences.xml",
-            at: "authorization.preferences.xml"
+            ct: "application/x-www-form-urlencoded", cj: "application/json", ua: "KakaoTalk Share Extension/9.1.7"
+        }, this.pa = null, this.mi = null, this.k = [
+            ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"], [10, 2, 3, -4, 20, 73, 47, -38, 27, -22, 11, -20, -22, 37, 36, 54],
+            [67, 109, -115, -110, -23, 119, 33, 86, -99, -28, -102, 109, -73, 13, 43, -96, 109, -76, 91, -83, 73, -14, 107, -88, 6, 11, 74, 109, 84, -68, -80, 15],
+            "dkljleskljfeisflssljeif "
+        ], this.p = {
+            pu: "hw.perferences.xml", at: "authorization.preferences.xml"
         }, this.d = '<string name="deviceUUID">', this.e = '<string name="encrypted_auth_token">';
     }
     return t.prototype.package = function(t) {
@@ -27,9 +23,7 @@ module.exports = function() {
     }, t.prototype.gDI = function() {
         let t = java.security.MessageDigest.getInstance("SHA");
         t.reset(), t.update(this.j(this.k[3] + this.r(this.pa + this.p.pu).split(this.d)[1].split("</string>")[0]).getBytes());
-        let e = t.digest(),
-            a = new java.lang.StringBuffer,
-            n = this.k[0];
+        let e = t.digest(), a = new java.lang.StringBuffer, n = this.k[0];
         for (let t = 0; t < e.length; t++) {
             let s = e[t];
             a.append(n[(240 & s) >> 4]), a.append(n[15 & s]);
